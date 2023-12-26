b1caf9aca6be1c3dd64baf559dac1954fda3406f
Timothetech <tounaottoutimothee@gmail.com>
2023-12-26 11:50:16 +0100

    Update index.blade.php

--- resources/views/front_office/index.blade.php
+++ resources/views/front_office/index.blade.php
@@ -765,10 +765,10 @@ $candidates = candidate::orderBy('dossard','asc')->get();
                                 <a class="btn btn-square btn-primary mx-1" href=""><i class="fab fa-facebook-f"></i></a>
                                 <a class="btn btn-square btn-primary mx-1" href=""><i class="fab fa-twitter"></i></a>
                                 <a class="btn btn-square btn-primary mx-1" href=""><i class="fab fa-instagram"></i></a>
-                            </div> --}}
+                            </div>
                         </div>
                     </div>
-                    @endforeach --}}
+                    @endforeach--}}
                     {{-- <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
                         <div class="team-item text-center rounded overflow-hidden">
                             <div class="rounded-circle overflow-hidden m-4">
